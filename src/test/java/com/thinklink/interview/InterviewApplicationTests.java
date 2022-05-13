68106841971becdcd186162bb44ca661e3150128
manoj <manoj@digitap.ai>
2022-05-13 22:19:23 +0530

    initial-commit

---
+++ src/test/java/com/thinklink/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinklink.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
